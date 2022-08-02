57436d725137979858e02bf18405779af92e9b4d
Nastya <himeriya97@mgmail.com>
2022-08-02 18:22:32 +0300

    feat: add answer

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,28 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        console.log(this.min, this.max)
     }
 
     guess() {
-
+        let guess_res = Math.round((this.max - this.min) / 2) + this.min
+        console.log(guess_res)
+        this.res = guess_res
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
+        console.log(this.min, this.max)
+        return this.max
     }
 
     greater() {
-
+        this.min = this.res;
+        console.log(this.min, this.max)
+        return this.min
     }
 }
 
